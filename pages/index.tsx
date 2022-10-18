46ac5b5e21c1811747f5afb4ff7a1c40f8ba2659
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2022-10-17 19:23:18 -0500

    GSAP Styles added

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,22 +6,129 @@ import doggoPic from "../public/doggo.png";
 import tigerPic from "../public/tiger.png";
 import tortoisePic from "../public/tortoise.png";
 import Link from "next/link";
+import { useEffect, useRef, useState } from "react";
+import { gsap } from "gsap";
 
 const HomePage: NextPage = () => {
+  const sectionRef = useRef(null);
+  const titleRef = useRef(null);
+  const text1 = useRef(null);
+  const text2 = useRef(null);
+  const [markup1, setMarkup1] = useState("wow");
+  const [title, setTitle] = useState("get fashion");
+
+  useEffect(() => {
+    let tl = gsap.timeline({});
+    let tl2 = gsap.timeline({ delay: 15, repeat: -1 });
+    let tl3 = gsap.timeline({ delay: 3, repeat: -1 });
+    tl.to(text2.current, { top: "30vh", opacity: 1, duration: 1 })
+      .to(text1.current, { right: "70%", opacity: 1, duration: 1 })
+      .to(sectionRef.current, { marginTop: "20rem", opacity: 1, duration: 1 })
+      .to(text1.current, { opacity: 0, right: "50%", duration: 2, delay: 10 });
+
+    tl2
+      .call(() => setMarkup1("brand"))
+      .to(text1.current, { left: "-100%", duration: 0.1 })
+      .to(text1.current, { opacity: 1, right: "60%", duration: 2 })
+
+      .to(text1.current, { opacity: 0, right: "50%", duration: 2, delay: 10 })
+      .call(() => setMarkup1("clothes"))
+      .to(text1.current, { left: "-130%", duration: 0.1 })
+      .to(text1.current, { opacity: 1, right: "60%", duration: 2 })
+
+      .to(text1.current, { opacity: 0, right: "50%", duration: 2, delay: 10 })
+      .call(() => setMarkup1("wow"))
+      .to(text1.current, { left: "-60%", duration: 0.1 })
+      .to(text1.current, { opacity: 1, right: "70%", duration: 2 })
+      .to(text1.current, { opacity: 0, right: "50%", duration: 2, delay: 10 });
+
+    tl3
+    .to(titleRef.current, { translateY: "-110%", duration: 1, delay:4 })
+    .call(() => setTitle("be fancy"))
+    .to(titleRef.current, { opacity: "0", duration: .1 })
+    .to(titleRef.current, { translateY: "110%", duration: .1 })
+    .to(titleRef.current, { opacity: "1", duration: .1 })
+    .to(titleRef.current, { translateY: "0", duration: 1 })
+
+    .to(titleRef.current, { translateY: "-110%", duration: 1, delay:4  })
+    .call(() => setTitle("new elegance"))
+    .to(titleRef.current, { opacity: "0", duration: .1 })
+    .to(titleRef.current, { translateY: "110%", duration: .1 })
+    .to(titleRef.current, { opacity: "1", duration: .1 })
+    .to(titleRef.current, { translateY: "0", duration: 1 })
+
+    .to(titleRef.current, { translateY: "-110%", duration: 1, delay:4  })
+    .call(() => setTitle("get fashion"))
+    .to(titleRef.current, { opacity: "0", duration: .1 })
+    .to(titleRef.current, { translateY: "110%", duration: .1 })
+    .to(titleRef.current, { opacity: "1", duration: .1 })
+    .to(titleRef.current, { translateY: "0", duration: 1 })
+
+
+
+    return () => {};
+  }, []);
+
   return (
     <div className={styles.container}>
-      <div className={styles.back_text}>wow</div>
-      <div className={styles.bg_text}>brown</div>
-      <section className={styles.main_content}>
-        <h1 className={styles.title}>get fashion</h1>
+      <div ref={text1} className={styles.back_text}>
+        <p>{markup1}</p>
+      </div>
+      <div ref={text2} className={styles.bg_text}>
+        <p>all new</p>
+      </div>
+      <section ref={sectionRef} className={styles.main_content}>
+        <div className={styles.text_hidden_wrapper}>
+          <h1 ref={titleRef} className={styles.title}>
+            {title}
+          </h1>
+        </div>
         <div className={styles.tiger}>
-        <Image  src={tigerPic} layout="fill" objectFit="cover" alt="Tiger picture that is really scary" />
+          <Image
+            src={tigerPic}
+            layout="fill"
+            objectFit="cover"
+            alt="Tiger picture that is really scary"
+          />
+          <div>
+            <p>
+              Sentiments two occasional affronting solicitude travelling and one
+              contrasted. Fortune day out married parties. Happiness remainder
+              joy but earnestly for off. Took sold add play may none him few. If
+              as increasing contrasted entreaties be. Now summer who day looked
+              our behind moment coming. Pain son rose more park way that. An
+              stairs as be lovers uneasy.
+            </p>
+          </div>
         </div>
         <div className={styles.doggo}>
-        <Image  src={doggoPic} layout="fill" objectFit="cover" alt="Doggo picture that is really happy" />
+          <Image
+            src={doggoPic}
+            layout="fill"
+            objectFit="cover"
+            alt="Doggo picture that is really happy"
+          />
+          <div>
+            <p>
+              Sentiments two occasional affronting solicitude travelling and one
+              contrasted. Fortune day out married parties. Happiness remainder
+              joy but earnestly for off. Took sold add play may none him few. If
+              as increasing contrasted entreaties be. Now summer who day looked
+              our behind moment coming. Pain son rose more park way that. An
+              stairs as be lovers uneasy.
+            </p>
+          </div>
         </div>
-        <div className={styles.tortoise} >
-        <Image src={tortoisePic} layout="fill" objectFit="cover" alt="Tortoise picture that is really calm" />
+        <div className={styles.tortoise}>
+          <Image
+            src={tortoisePic}
+            layout="fill"
+            objectFit="cover"
+            alt="Tortoise picture that is really calm"
+          />
+          <div>
+            <p>Sentiments two occasional affronting solicitude.</p>
+          </div>
         </div>
         <div className={styles.content}>
           <p className={styles.text_content}>
@@ -31,7 +138,8 @@ const HomePage: NextPage = () => {
           <Link href="#">
             <div className={styles.link_wrapper}>
               <a className={styles.shop_link}>shop wow</a>
-              <svg style={{cursor: 'pointer'}}
+              <svg
+                style={{ cursor: "pointer" }}
                 width="258"
                 height="8"
                 viewBox="0 0 258 8"
