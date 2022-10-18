cd4873653270b5c45786761cd330115b437d5140
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2022-10-17 19:31:32 -0500

    SEO Tags Added

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,119 +43,142 @@ const HomePage: NextPage = () => {
       .to(text1.current, { opacity: 0, right: "50%", duration: 2, delay: 10 });
 
     tl3
-    .to(titleRef.current, { translateY: "-110%", duration: 1, delay:4 })
-    .call(() => setTitle("be fancy"))
-    .to(titleRef.current, { opacity: "0", duration: .1 })
-    .to(titleRef.current, { translateY: "110%", duration: .1 })
-    .to(titleRef.current, { opacity: "1", duration: .1 })
-    .to(titleRef.current, { translateY: "0", duration: 1 })
+      .to(titleRef.current, { translateY: "-110%", duration: 1, delay: 4 })
+      .call(() => setTitle("be fancy"))
+      .to(titleRef.current, { opacity: "0", duration: 0.1 })
+      .to(titleRef.current, { translateY: "110%", duration: 0.1 })
+      .to(titleRef.current, { opacity: "1", duration: 0.1 })
+      .to(titleRef.current, { translateY: "0", duration: 1 })
 
-    .to(titleRef.current, { translateY: "-110%", duration: 1, delay:4  })
-    .call(() => setTitle("new elegance"))
-    .to(titleRef.current, { opacity: "0", duration: .1 })
-    .to(titleRef.current, { translateY: "110%", duration: .1 })
-    .to(titleRef.current, { opacity: "1", duration: .1 })
-    .to(titleRef.current, { translateY: "0", duration: 1 })
+      .to(titleRef.current, { translateY: "-110%", duration: 1, delay: 4 })
+      .call(() => setTitle("new elegance"))
+      .to(titleRef.current, { opacity: "0", duration: 0.1 })
+      .to(titleRef.current, { translateY: "110%", duration: 0.1 })
+      .to(titleRef.current, { opacity: "1", duration: 0.1 })
+      .to(titleRef.current, { translateY: "0", duration: 1 })
 
-    .to(titleRef.current, { translateY: "-110%", duration: 1, delay:4  })
-    .call(() => setTitle("get fashion"))
-    .to(titleRef.current, { opacity: "0", duration: .1 })
-    .to(titleRef.current, { translateY: "110%", duration: .1 })
-    .to(titleRef.current, { opacity: "1", duration: .1 })
-    .to(titleRef.current, { translateY: "0", duration: 1 })
-
-
+      .to(titleRef.current, { translateY: "-110%", duration: 1, delay: 4 })
+      .call(() => setTitle("get fashion"))
+      .to(titleRef.current, { opacity: "0", duration: 0.1 })
+      .to(titleRef.current, { translateY: "110%", duration: 0.1 })
+      .to(titleRef.current, { opacity: "1", duration: 0.1 })
+      .to(titleRef.current, { translateY: "0", duration: 1 });
 
     return () => {};
   }, []);
 
   return (
-    <div className={styles.container}>
-      <div ref={text1} className={styles.back_text}>
-        <p>{markup1}</p>
-      </div>
-      <div ref={text2} className={styles.bg_text}>
-        <p>all new</p>
-      </div>
-      <section ref={sectionRef} className={styles.main_content}>
-        <div className={styles.text_hidden_wrapper}>
-          <h1 ref={titleRef} className={styles.title}>
-            {title}
-          </h1>
+    <>
+      <Head>
+        <title>NextJs with GSAP</title>
+        <meta
+          property="og:title"
+          content="NextJs with GSAP using Typescript"
+          key="title"
+        />
+        <meta
+          name="description"
+          content="Intro website using NextJs and GSAP, intro for personal portfolio, hope you enjoy watching my website"
+        />
+        <meta
+          name="keywords"
+          content="nextjs react typescript gsap animation transitions landing page"
+        />
+        <meta name="author" content="Diego Mejia" />
+        <link
+          rel="canonical"
+          href="https://nextjs-gsap-intro-page-1.vercel.app/"
+        />
+        <meta name="robots" content="follow" />
+        <meta name="robots" content="index" />
+      </Head>
+      <div className={styles.container}>
+        <div ref={text1} className={styles.back_text}>
+          <p>{markup1}</p>
         </div>
-        <div className={styles.tiger}>
-          <Image
-            src={tigerPic}
-            layout="fill"
-            objectFit="cover"
-            alt="Tiger picture that is really scary"
-          />
-          <div>
-            <p>
-              Sentiments two occasional affronting solicitude travelling and one
-              contrasted. Fortune day out married parties. Happiness remainder
-              joy but earnestly for off. Took sold add play may none him few. If
-              as increasing contrasted entreaties be. Now summer who day looked
-              our behind moment coming. Pain son rose more park way that. An
-              stairs as be lovers uneasy.
-            </p>
+        <div ref={text2} className={styles.bg_text}>
+          <p>all new</p>
+        </div>
+        <section ref={sectionRef} className={styles.main_content}>
+          <div className={styles.text_hidden_wrapper}>
+            <h1 ref={titleRef} className={styles.title}>
+              {title}
+            </h1>
           </div>
-        </div>
-        <div className={styles.doggo}>
-          <Image
-            src={doggoPic}
-            layout="fill"
-            objectFit="cover"
-            alt="Doggo picture that is really happy"
-          />
-          <div>
-            <p>
-              Sentiments two occasional affronting solicitude travelling and one
-              contrasted. Fortune day out married parties. Happiness remainder
-              joy but earnestly for off. Took sold add play may none him few. If
-              as increasing contrasted entreaties be. Now summer who day looked
-              our behind moment coming. Pain son rose more park way that. An
-              stairs as be lovers uneasy.
-            </p>
-          </div>
-        </div>
-        <div className={styles.tortoise}>
-          <Image
-            src={tortoisePic}
-            layout="fill"
-            objectFit="cover"
-            alt="Tortoise picture that is really calm"
-          />
-          <div>
-            <p>Sentiments two occasional affronting solicitude.</p>
-          </div>
-        </div>
-        <div className={styles.content}>
-          <p className={styles.text_content}>
-            SHOP THE NEW FALL COLLECTION OF WOW APPAREAL. THE HOTTEST TRENDS,
-            THE LATEST VIBES, AND THE BEST ECO-FRIENDLY QUALITY.
-          </p>
-          <Link href="#">
-            <div className={styles.link_wrapper}>
-              <a className={styles.shop_link}>shop wow</a>
-              <svg
-                style={{ cursor: "pointer" }}
-                width="258"
-                height="8"
-                viewBox="0 0 258 8"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M257.354 4.35355C257.549 4.15829 257.549 3.84171 257.354 3.64645L254.172 0.464466C253.976 0.269204 253.66 0.269204 253.464 0.464466C253.269 0.659728 253.269 0.976311 253.464 1.17157L256.293 4L253.464 6.82843C253.269 7.02369 253.269 7.34027 253.464 7.53553C253.66 7.7308 253.976 7.7308 254.172 7.53553L257.354 4.35355ZM0 4.5H257V3.5H0V4.5Z"
-                  fill="white"
-                />
-              </svg>
+          <div className={styles.tiger}>
+            <Image
+              src={tigerPic}
+              layout="fill"
+              objectFit="cover"
+              alt="Tiger picture that is really scary"
+            />
+            <div>
+              <p>
+                Sentiments two occasional affronting solicitude travelling and
+                one contrasted. Fortune day out married parties. Happiness
+                remainder joy but earnestly for off. Took sold add play may none
+                him few. If as increasing contrasted entreaties be. Now summer
+                who day looked our behind moment coming. Pain son rose more park
+                way that. An stairs as be lovers uneasy.
+              </p>
             </div>
-          </Link>
-        </div>
-      </section>
-    </div>
+          </div>
+          <div className={styles.doggo}>
+            <Image
+              src={doggoPic}
+              layout="fill"
+              objectFit="cover"
+              alt="Doggo picture that is really happy"
+            />
+            <div>
+              <p>
+                Sentiments two occasional affronting solicitude travelling and
+                one contrasted. Fortune day out married parties. Happiness
+                remainder joy but earnestly for off. Took sold add play may none
+                him few. If as increasing contrasted entreaties be. Now summer
+                who day looked our behind moment coming. Pain son rose more park
+                way that. An stairs as be lovers uneasy.
+              </p>
+            </div>
+          </div>
+          <div className={styles.tortoise}>
+            <Image
+              src={tortoisePic}
+              layout="fill"
+              objectFit="cover"
+              alt="Tortoise picture that is really calm"
+            />
+            <div>
+              <p>Sentiments two occasional affronting solicitude.</p>
+            </div>
+          </div>
+          <div className={styles.content}>
+            <p className={styles.text_content}>
+              SHOP THE NEW FALL COLLECTION OF WOW APPAREAL. THE HOTTEST TRENDS,
+              THE LATEST VIBES, AND THE BEST ECO-FRIENDLY QUALITY.
+            </p>
+            <Link href="#">
+              <div className={styles.link_wrapper}>
+                <a className={styles.shop_link}>shop wow</a>
+                <svg
+                  style={{ cursor: "pointer" }}
+                  width="258"
+                  height="8"
+                  viewBox="0 0 258 8"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M257.354 4.35355C257.549 4.15829 257.549 3.84171 257.354 3.64645L254.172 0.464466C253.976 0.269204 253.66 0.269204 253.464 0.464466C253.269 0.659728 253.269 0.976311 253.464 1.17157L256.293 4L253.464 6.82843C253.269 7.02369 253.269 7.34027 253.464 7.53553C253.66 7.7308 253.976 7.7308 254.172 7.53553L257.354 4.35355ZM0 4.5H257V3.5H0V4.5Z"
+                    fill="white"
+                  />
+                </svg>
+              </div>
+            </Link>
+          </div>
+        </section>
+      </div>
+    </>
   );
 };
 
